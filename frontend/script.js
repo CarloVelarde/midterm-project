99acd57e522cc0ca07f9d3f3ef22a991ffba8dca
CarloVelarde <cav2003@live.com>
2024-03-04 21:44:22 -0600

    added front end functionality except for edit

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,86 @@
-let cityInput = document.getElementById('cityName')
+let form = document.getElementById('form-add')
+let cityInput = document.getElementById('title')
 let ratingInput = document.getElementById('rating')
 let descInput = document.getElementById('desc')
 
 let reviews = document.getElementById('reviews')
+let submitButton = document.getElementById('submitButton')
+let deleteButton = document.getElementById('')
+
 let data = []
 let selectedReview = {}
+
 const api = 'http://127.0.0.1:8000/'
+
+
+// Display reviews
+function fetchReviews() {
+   fetch(`${api}travels`)
+      .then(response => response.json())
+      .then(items => {
+         reviews.innerHTML = '' // Clear current reviews
+         items.forEach(review => {
+            reviews.innerHTML += `
+           <div class="review mt-3" data-city="${review.city.toLowerCase()}">
+               <h3>${review.city}</h3>
+               <p>Rating: ${review.rating}</p>
+               <p>${review.description}</p>
+               <button class = "btn btn-outline-primary" onclick="deleteReview('${review.city.toLowerCase()}')">Delete</button>
+               <button class = "btn btn-secondary" onclick="editReview('${review.city}')">Edit</button>
+           </div>
+       `;
+         })
+      })
+}
+
+// Add reviews
+function addReview(){
+   const newTravel = {
+      city: cityInput.value,
+      rating: ratingInput.value,
+      description: descInput.value 
+   }
+
+   fetch(`${api}travels`,{
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTravel),
+   }).then(() => {
+      fetchReviews();
+      cityInput.value = ''
+      ratingInput.value = ''
+      descInput.value = ''
+      
+   });
+}
+
+
+// Delete reviews
+function deleteReview(city){
+   fetch(`${api}travels/${city}`,{
+      method: 'DELETE',
+   }).then(() => {
+      fetchReviews()
+   })
+}
+
+
+
+// Event listeners
+submitButton.addEventListener('click', addReview)
+
+// Initial fetch of items
+document.addEventListener('DOMContentLoaded', fetchReviews);
+
+
+
+
+
+
+
+
 
 
 
--- model.py
+++ model.py
@@ -1,12 +1,26 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class Travel(BaseModel):
    city: str
    rating: int 
    description: str 
 
+   # Ensures ratings between 1-5
+   @validator('rating')
+   def validate_rating(cls, value):
+       if not 1 <= value <= 5:
+           raise ValueError('Rating must be between 1 and 5')
+       return value
+
 class TravelRequest(BaseModel):
    city: Optional[str] = None
    rating: Optional[int] = None
    description: Optional[str] = None
+
+   # Ensures ratings between 1-5
+   @validator('rating', allow_reuse=True)
+   def validate_rating(cls, value):
+      if value is not None and not 1 <= value <= 5:
+         raise ValueError('Rating must be between 1 and 5')
+      return value
